21d37082d2a487dba761c9de17a6bd5ae4ea9131
Maksio ... <lukaszmaks05@gmail.com>
2024-12-12 13:00:08 +0100

    Correct Add board game

--- View/AddBoardGame.xaml.cs
+++ View/AddBoardGame.xaml.cs
@@ -109,7 +109,7 @@ namespace Boardgame.View
 
         private void People_LostFocus(object sender, RoutedEventArgs e)
         {
-            if (string.IsNullOrWhiteSpace(Description.Text))
+            if (string.IsNullOrWhiteSpace(People.Text))
             {
                 People.Text = "People...";
                 People.Foreground = new SolidColorBrush(Color.FromRgb(224, 225, 241)); // Przywracamy szary kolor czcionki
@@ -127,10 +127,10 @@ namespace Boardgame.View
 
         private void Hours_LostFocus(object sender, RoutedEventArgs e)
         {
-            if (string.IsNullOrWhiteSpace(Description.Text))
+            if (string.IsNullOrWhiteSpace(Hours.Text))
             {
-                Description.Text = "Hours...";
-                Description.Foreground = new SolidColorBrush(Color.FromRgb(224, 225, 241)); // Przywracamy szary kolor czcionki
+                Hours.Text = "Hours...";
+                Hours.Foreground = new SolidColorBrush(Color.FromRgb(224, 225, 241)); // Przywracamy szary kolor czcionki
             }
         }
     }
